9dc64bdb4d925015e94caf05eca8ad03d73e6e63
emakedon <emakedon1@gmail.com>
2022-01-21 17:26:34 -0600

    lab3 done

---
+++ lab03/answers.sql
@@ -0,0 +1,105 @@
+-- Exercise 1 (done for you):
+SELECT * FROM users;
+
+
+
+-- Exercise 2 (done for you):
+SELECT id, first_name, last_name
+FROM users;
+
+
+
+-- Exercise 3
+SELECT id, first_name, last_name
+FROM users
+ORDER BY last_name;
+
+
+
+-- Exercise 4
+SELECT id, user_id, image_url
+FROM posts
+WHERE user_id=26;
+
+
+
+-- Exercise 5
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id=26
+OR user_id=12;
+
+
+
+-- Exercise 6
+SELECT COUNT(posts)
+FROM posts;
+
+
+
+-- Exercise 7
+SELECT user_id, COUNT(user_id)
+FROM "comments"
+GROUP BY user_id
+ORDER BY COUNT(user_id) DESC;
+
+
+
+-- Exercise 8
+SELECT posts.id, posts.image_url, user_id, username, first_name, last_name
+FROM posts
+INNER JOIN users
+ON posts.user_id = users.id
+WHERE user_id=26
+OR user_id=12;
+
+
+
+-- Exercise 9
+SELECT posts.id, pub_date, following_id
+FROM following
+INNER JOIN posts
+ON following.following_id = posts.user_id
+WHERE following.user_id=26;
+
+
+
+-- Exercise 10
+SELECT posts.id, pub_date, following_id, username
+FROM following
+INNER JOIN posts ON following.following_id = posts.user_id
+INNER JOIN users ON following.following_id = users.id
+WHERE following.user_id=26
+ORDER BY pub_date DESC;
+
+
+
+-- Exercise 11
+INSERT INTO bookmarks (user_id, post_id)
+VALUES ('26','219');
+INSERT INTO bookmarks (user_id, post_id)
+VALUES ('26','220');
+INSERT INTO bookmarks (user_id, post_id)
+VALUES ('26','221');
+
+
+
+-- Exercise 12
+DELETE FROM bookmarks
+WHERE post_id='219';
+DELETE FROM bookmarks
+WHERE post_id='220';
+DELETE FROM bookmarks
+WHERE post_id='221';
+
+
+
+
+-- Exercise 13
+UPDATE users
+SET email = 'cyoung2022@gmail.com'
+WHERE id = 26;
+
+
+
+-- Exercise 14
